bcee59d175ee041552870f85a932500bd3b6806b
Shreya75 <150461366+Shreya-75@users.noreply.github.com>
2025-01-28 02:06:12 +0530

    Add files via upload

---
+++ Data/HospitalContext.cs
@@ -0,0 +1,15 @@
+﻿using HospitalManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagement.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext(DbContextOptions<HospitalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; } 
+    }
+}
